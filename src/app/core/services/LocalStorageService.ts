87dbffcbaed2b78d094c8f0a466886af9e16dfd8
Oleksandr P <uaman89@gmail.com>
2023-08-04 15:25:34 +0300

    feat(StorageService):  add method to get list of existed records

--- src/app/core/services/LocalStorageService.ts
+++ src/app/core/services/LocalStorageService.ts
@@ -4,6 +4,12 @@ import { IngredientsGroup } from '../interfaces/ingredients-group.interace';
 
 @Injectable()
 export class LocalStorageService implements StorageService {
+  readonly savedPlansKey = '__saved-plans-ids';
+
+  constructor(){
+
+  }
+
   save(planId: string, data: object) {
     localStorage.setItem(planId, JSON.stringify(data));
   }
@@ -11,5 +17,16 @@ export class LocalStorageService implements StorageService {
   load(planId: string): IngredientsGroup[] {
     const savedData = localStorage.getItem(planId);
     return savedData ? (JSON.parse(savedData) as IngredientsGroup[]) : [];
+  }
+
+  getSavedPlanList(): string[] {
+    const value = localStorage.getItem(this.savedPlansKey);
+    return value ? JSON.parse(value) : [];
+  }
+
+  private updateSavedIdList(newId:string){
+    const keysList = this.getSavedPlanList();
+    keysList.push(newId);
+    localStorage.setItem(this.savedPlansKey, JSON.stringify(keysList));
   }
 }
